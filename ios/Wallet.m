0f5ac614b1c4c20375db49dab7f3e027fa10243b
Rômulo Dusi <dusiromulo@gmail.com>
2019-11-21 15:08:09 -0300

    copia codigo lib antiga

--- ios/Wallet.m
+++ ios/Wallet.m
@@ -13,14 +13,91 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod2:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
-}
-
 RCT_EXPORT_METHOD(canAddPasses:(RCTResponseSenderBlock)callback) {
 	callback(@[@([PKAddPassesViewController canAddPasses])]);
+}
+
+RCT_EXPORT_METHOD(
+                  showAddPassControllerFromFile:(NSString *)filepath
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  ) {
+    NSData* data =[[NSData alloc]init];
+
+    data = [NSData dataWithContentsOfFile:filepath];
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self showViewControllerWithData:data resolver:resolve rejecter:reject];
+    });
+
+}
+
+RCT_EXPORT_METHOD(
+	showAddPassControllerFromURL:(NSString *)pass
+	resolver:(RCTPromiseResolveBlock)resolve
+	rejecter:(RCTPromiseRejectBlock)reject
+) {
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+		NSURL *passURL = [[NSURL alloc] initWithString:pass];
+		if (!passURL) {
+			reject(kRejectCode, @"The pass URL is invalid", nil);
+			return;
+		}
+
+		NSData *data = [[NSData alloc] initWithContentsOfURL:passURL];
+		if (!data) {
+			reject(kRejectCode, @"The pass data is invalid", nil);
+			return;
+		}
+
+		[self showViewControllerWithData:data resolver:resolve rejecter:reject];
+	});
+}
+
+- (void)showViewControllerWithData:(NSData *)data
+                          resolver:(RCTPromiseResolveBlock)resolve
+                          rejecter:(RCTPromiseRejectBlock)reject {
+    NSError *passError;
+    self.pass = [[PKPass alloc] initWithData:data error:&passError];
+
+    if (passError) {
+        reject(kRejectCode, @"The pass is invalid", passError);
+        return;
+    }
+
+    self.passLibrary = [[PKPassLibrary alloc] init];
+    if ([self.passLibrary containsPass:self.pass]) {
+        resolve(@(YES));
+        return;
+    }
+
+    UIViewController *viewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+
+    PKAddPassesViewController *passController = [[PKAddPassesViewController alloc] initWithPass:self.pass];
+    passController.delegate = self;
+    self.resolveBlock = resolve;
+
+    while (viewController.presentedViewController) {
+        viewController = viewController.presentedViewController;
+    }
+
+    [viewController presentViewController:passController animated:YES completion:nil];
+}
+
+#pragma mark - PKAddPassesViewControllerDelegate
+
+- (void)addPassesViewControllerDidFinish:(PKAddPassesViewController *)controller
+{
+	[controller dismissViewControllerAnimated:YES completion:^{
+		if (self.resolveBlock) {
+			self.resolveBlock(@([self.passLibrary containsPass:self.pass]));
+            self.resolveBlock = nil;
+		}
+
+		controller.delegate = nil;
+		self.passLibrary = nil;
+		self.pass = nil;
+	}];
 }
 
 @end
